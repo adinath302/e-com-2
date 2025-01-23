235c9c14de8417b4231be7e443b224248cf3aa82
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-01-23 21:46:16 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from "./component/Navbar/Navbar.jsx"
+
+const App = () => {
+  return <div className='bg-red-500'>
+    <Navbar/>
+  </div>
+}
+
+export default App
